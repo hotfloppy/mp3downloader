bd997a28e6e2bf9bd61db5ec53b720dfc04f1b58
Pencinta Kucing <hotfloppy.6866@gmail.com>
2021-07-16 18:04:23 +0800

    adding some checking during installation

--- install.sh
+++ install.sh
@@ -7,6 +7,25 @@ eval $lines
 echo "Making sure $HOME/bin exist.."
 mkdir -p $HOME/bin
 
+if [[ ! $(echo $PATH | grep $HOME/bin) ]]; then
+  case $SHELL in
+    /usr/bin/bash)
+      myrc="$HOME/.bashrc"
+      echo "export PATH=$PATH:$HOME/bin" >> "$myrc"
+      ;;
+    /usr/bin/zsh)
+      myrc="$HOME/.zshrc"
+      echo "export PATH=$PATH:$HOME/bin" >> "$myrc"
+      ;;
+    /usr/bin/fish)
+      myrc="$HOME/.config/fish/config.fish"
+      echo "set PATH $PATH $HOME/bin" >> "$myrc"
+      ;;
+    *) echo "Unknown shell. Please manually add $HOME/bin to your PATH." ;;
+  esac
+  source "$myrc"
+fi
+
 eval $lines
 echo "Installing mp3downloader into $HOME/bin.."
 ln -s ${basedir}/mp3downloader $HOME/bin/
